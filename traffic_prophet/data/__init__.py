3001dc9a63425b64d988316cbb2793f433cc7d00
cczhu <chenchong.zhu@gmail.com>
2019-10-20 00:39:16 -0400

    #8 Added additional counts from 2011 and 2012 and lon-lats for count stations to sample files.

--- traffic_prophet/data/__init__.py
+++ traffic_prophet/data/__init__.py
@@ -8,5 +8,11 @@ def _full_path(name, dirname=_path.dirname(_path.abspath(__file__))):
     return _path.join(dirname, name)
 
 
-SAMPLE_ZIP = _full_path('15_min_counts_2010_neg_sample.zip')
+SAMPLE_ZIP = {
+    '2010': _full_path('15_min_counts_2010_neg_sample.zip'),
+    '2011': _full_path('15_min_counts_2011_neg_sample.zip'),
+    '2012': _full_path('15_min_counts_2011_neg_sample.zip')}
+"""Sample zip of 15-minute bin count files from 2010, 2011 and 2012."""
+
+SAMPLE_LONLAT = _full_path('count_lonlat.csv')
 """Sample zip of 15-minute bin count files."""
