8436d6b44b9d891da964d955c46791de75d3e4d8
cczhu <chenchong.zhu@gmail.com>
2019-10-21 01:19:40 -0400

    #8 Some further refactoring, and working on test suite for reader classes.

--- traffic_prophet/countmatch/tests/test_reader.py
+++ traffic_prophet/countmatch/tests/test_reader.py
@@ -6,6 +6,72 @@ from ...data import SAMPLE_ZIP
 from .. import reader
 
 
+class TestAnnualCount:
+    """Test preprocessing routines in AnnualCount."""
+
+    def setup(self):
+        rdr = reader.Reader(SAMPLE_ZIP)
+        zr = rdr.get_zipreader(SAMPLE_ZIP['2010'])
+        self.counts = list(zr)
+        self.sttc_data = self.counts[2]
+        self.ptc_data = self.counts[7]
+
+    def test_regularize_timeseries(self):
+        # Test processing a file that has no rounding issues.
+        ac = reader.AnnualCount(1000, -1, 2010, None)
+        crd = ac.regularize_timeseries(self.ptc_data)
+        for key in ['Timestamp', 'Date', 'Month', 'Day of Week', 'Count']:
+            assert key in crd.columns
+        assert crd.shape == (self.ptc_data['data'].shape[0], 5)
+        assert np.array_equal(crd['Timestamp'].dt.minute.unique(),
+                              np.array([0, 15, 30, 45]))
+        assert np.all(crd['Count'] ==
+                      self.ptc_data['data']['Count'].astype(float))
+        assert np.all(crd['Day of Week'] ==
+                      self.ptc_data['data']['Timestamp'].dt.dayofweek)
+
+        # Introduce some errors to sttc_data.
+        fake_sttc_data = self.sttc_data.copy()
+        # Do a deep copy just in case.
+        fake_sttc_data['data'] = self.sttc_data['data'].copy()
+        fake_sttc_data['data'].at[30, 'Timestamp'] = (
+            pd.Timestamp('2010-06-09 07:39:31'))
+        fake_sttc_data['data'].at[30, 'Count'] = 235
+        fake_sttc_data['data'].at[32, 'Timestamp'] = (
+            pd.Timestamp('2010-06-09 07:51:12'))
+        crd2 = ac.regularize_timeseries(fake_sttc_data)
+        assert crd2.shape == (self.sttc_data['data'].shape[0] - 2, 5)
+        assert np.array_equal(crd2['Timestamp'].dt.minute.unique(),
+                              np.array([0, 15, 30, 45]))
+        assert crd2.at[30, 'Timestamp'] == pd.Timestamp('2010-06-09 07:45:00')
+        assert np.isclose(crd2.at[30, 'Count'], 93., rtol=1e-10)
+
+    def test_process_count_data(self):
+        ac = reader.AnnualCount(1000, -1, 2010, None)
+        crd = ac.regularize_timeseries(self.ptc_data)
+        daily_counts = ac.process_count_data(crd)
+
+        # Ensure every unique date is represented.
+        assert daily_counts.shape == (crd['Date'].unique().shape[0], 1)
+        assert not np.any(daily_counts.index.duplicated())
+
+        # Fuzz test to see if we've summed the counts up properly.
+        for ctime in np.random.choice(daily_counts.index.values, size=10):
+            assert np.isclose(
+                daily_counts.at[ctime, 'Daily Count'],
+                crd.loc[crd['Date'] == ctime, 'Count'].sum(),
+                rtol=1e-10)
+
+    def test_is_permanent(self):
+        known_ptc_ids = [890, 104870]
+        ac = reader.AnnualCount(1000, -1, 2010, None)
+        for c in self.counts:
+            if c['centreline_id'] in known_ptc_ids:
+                assert ac.is_permanent_count(self.ptc_data)
+            else:
+                assert not ac.is_permanent_count(self.sttc_data)
+
+
 class TestReader:
     """Test reader.Reader methods."""
 
@@ -41,6 +107,15 @@ class TestReader:
         counts = list(zr)
 
         assert len(counts) == 8
+        assert ([c['filename'] for c in counts] ==
+                ['104870_11510_2010.txt',
+                 '241_4372_2010.txt',
+                 '252_4505_2010.txt',
+                 '410_8108_2010.txt',
+                 '427_8256_2010.txt',
+                 '446378_2398_2010.txt',
+                 '487_9229_2010.txt',
+                 '890_17700_2010.txt'])
         assert ([c['centreline_id'] for c in counts] ==
                 [104870, 241, 252, 410, 427, 446378, 487, 890])
         assert ([c['direction'] for c in counts] ==
@@ -66,13 +141,3 @@ class TestReader:
             path = "/".join(SAMPLE_ZIP['2010'].split("/")[:-1]) + "/"
             rdr = reader.Reader(path + 'count_lonlat.csv')
             counts = rdr.read_zip()
-
-# class TestAnnualDailyCount:
-
-#     @pytest.mark.parametrize('filenumber', list(range())
-#     def test_round_timestamp(self, filenumber):
-#         dc = cm.AnnualDailyCount(0, 0, None)
-#         # Ensure randomly drawn data has same resolution as actual input data.
-#         t_pd = pd.to_datetime(t).round('1s')
-
-#         assert abs((t_rounded - t_pd) / np.timedelta64(1, 'm')) <= 7.5
