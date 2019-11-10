56e85fa58c398c60e8cb300a97c9d50d51594f50
cczhu <chenchong.zhu@gmail.com>
2019-11-10 16:31:20 -0500

    #15 basic class to handle Postgres connections.

--- traffic_prophet/connection.py
+++ traffic_prophet/connection.py
@@ -1,8 +1,20 @@
 """Base classes for connecting to Postgres database."""
 
+import psycopg2
+import configparser
+
+from . import cfg
+
 
 class Connection:
+    # TO DO: this currently has NO FORMAL TEST SUITE, mainly because we'll
+    # be replacing it with whatever Kedro cooked up at some point.
 
-    def __init__(self):
-        raise NotImplementedError('psycopg2 connection mechanism is not '
-                                  'yet implemented!')
+    def __init__(self, credential_file, credential_name):
+        config = configparser.RawConfigParser()
+        config.read(credential_file.as_posix())
+        self.credentials = dict(config[credential_name])
+        self.dbname = cfg.postgres['database']
+
+    def connect(self):
+        return psycopg2.connect(database=self.dbname, **self.credentials)
