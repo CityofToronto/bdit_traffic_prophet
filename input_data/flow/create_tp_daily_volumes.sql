0c8c029a06cc1f99d7551fd6216236ce23404b7a
Charles Zhu <charles.zhu@toronto.ca>
2021-07-12 08:37:39 -0400

    Properly name daily volumes table.

--- input_data/flow/create_tp_daily_volumes.sql
+++ input_data/flow/create_tp_daily_volumes.sql
@@ -1,8 +1,8 @@
 -- You must run create_tp_centreline_volumes.sql first.
 
--- DROP MATERIALIZED VIEW prj_volume.tp_centreline_daily_counts;
+-- DROP MATERIALIZED VIEW prj_volume.tp_daily_volumes;
 
-CREATE MATERIALIZED VIEW prj_volume.tp_centreline_daily_counts
+CREATE MATERIALIZED VIEW prj_volume.tp_daily_volumes
 TABLESPACE pg_default
 AS
  WITH step_1 AS (
@@ -41,10 +41,10 @@ AS
   ORDER BY (date_part('YEAR'::text, count_date)), centreline_id, dir_bin, count_date
 WITH DATA;
 
-ALTER TABLE prj_volume.tp_centreline_daily_counts
+ALTER TABLE prj_volume.tp_daily_volumes
     OWNER TO prj_volume_admins;
 
-COMMENT ON MATERIALIZED VIEW prj_volume.tp_centreline_daily_counts
+COMMENT ON MATERIALIZED VIEW prj_volume.tp_daily_volumes
     IS 'Aggregation of tp_centreline_volumes data after 2006-01-01 inclusive to daily bins, averaging duplicate bins and excluding any days of incomplete data. For TEPS and Traffic Prophet.';
 
-GRANT SELECT ON TABLE prj_volume.tp_centreline_daily_counts TO bdit_humans;
+GRANT SELECT ON TABLE prj_volume.tp_daily_volumes TO bdit_humans;
